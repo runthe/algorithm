9f74152bf2d5d036ab6d4bd805498a61f0a09679
soo <runthe@naver.com>
2017-11-05 12:51:30 +0900

    #바이러스 알고리즘 수정

--- src/soo/code/Virus.java
+++ src/soo/code/Virus.java
@@ -1,65 +1,72 @@
 package soo.code;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Virus {
 
-  private static Map<Integer, Set<Integer>> computerEdges = new HashMap<>();
+  private static List<int[]> computerEdges = new ArrayList<>();
 
   private static boolean[] countCheckArrays;
+
+  private static int totalComputerCount;
 
   private static int infectedComputerCount;
 
   public static void main(String[] args) {
     initialize();
-    getInfectedComputerCount(computerEdges.get(1));
+
+    countCheckArrays[0] = true;
+    calculateInfectedComputerCount(1);
+
     System.out.println(infectedComputerCount);
   }
 
-  private static void getInfectedComputerCount(Set<Integer> computers) {
-    for (int computer : computers) {
+  private static void calculateInfectedComputerCount(int infectedComputer) {
+    for (int[] computers : computerEdges) {
+      if (totalComputerCount == infectedComputerCount + 1) {
+        break;
+      }
 
-      if (!countCheckArrays[computer - 1]) {
-        countCheckArrays[computer - 1] = true;
+      int firstComputer = computers[0];
+      int secondComputer = computers[1];
+      int sideComputer = 0;
+
+      if (firstComputer == infectedComputer) {
+        sideComputer = secondComputer;
+      }
+
+      if (secondComputer == infectedComputer) {
+        sideComputer = firstComputer;
+      }
+
+      if (isWillInfectComputer(sideComputer)) {
+        countCheckArrays[sideComputer - 1] = true;
         infectedComputerCount++;
-
-        if (computerEdges.containsKey(computer)) {
-          getInfectedComputerCount(computerEdges.get(computer));
-        }
+        calculateInfectedComputerCount(sideComputer);
       }
     }
+  }
+
+  private static boolean isWillInfectComputer(int computer) {
+    return computer != 0 && !countCheckArrays[computer - 1];
   }
 
   private static void initialize() {
     Scanner sc = new Scanner(System.in);
 
-    countCheckArrays = new boolean[sc.nextInt()];
+    totalComputerCount = sc.nextInt();
+    countCheckArrays = new boolean[totalComputerCount];
     int computerEdgeCount = sc.nextInt();
 
     for (int i = 0; i < computerEdgeCount; i++) {
-      int edgeArrays[];
+      int edgeArray[];
       int firstComputer = sc.nextInt();
       int secondComputer = sc.nextInt();
 
-      edgeArrays = new int[]{firstComputer, secondComputer};
-      Arrays.sort(edgeArrays);
-
-      if (computerEdges.containsKey(edgeArrays[0])) {
-        Set<Integer> set = computerEdges.get(edgeArrays[0]);
-        set.add(edgeArrays[1]);
-
-        computerEdges.put(edgeArrays[0], set);
-      } else {
-        Set<Integer> set = new HashSet<>();
-        set.add(edgeArrays[1]);
-
-        computerEdges.put(edgeArrays[0], set);
-      }
+      edgeArray = new int[]{firstComputer, secondComputer};
+      computerEdges.add(edgeArray);
     }
   }
 }
